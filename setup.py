91155625631da0aed9398ba921a3da86c56d36c7
anthony0727 <jwseok0727@gmail.com>
2022-10-14 12:18:40 +0900

    overall layout

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name='deflector_gym',
     version='0.0.0',
-    url='https://github.com/kc-ml2/deflector-env',
+    url='https://github.com/kc-ml2/deflector-gym',
     author='KC ML2',
     author_email='anthony@kc-ml2.com',
     packages=find_packages(),
